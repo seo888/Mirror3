94be6d7d3b35b6fbd8cbf2935a27384ad00b83dd
Seo <53137698+seo888@users.noreply.github.com>
2023-12-25 11:23:31 +0000

    update

---
+++ lock/pyarmor_runtime_004374/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.5 (basic), 004374, 2023-12-25T11:23:09.505638
+from .pyarmor_runtime import __pyarmor__
--- pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 8.4.5 (trial), 000000, 2023-12-23T06:58:01.662206
-from .pyarmor_runtime import __pyarmor__
